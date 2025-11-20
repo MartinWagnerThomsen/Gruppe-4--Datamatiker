003f663422816964694c2d0c6c0295adbd012cc9
Esp <duckerlicious@duck.com>
2025-11-20 11:07:50 +0100

    F

--- Investeringsklubben/src/DataObjects/Stock.java
+++ Investeringsklubben/src/DataObjects/Stock.java
@@ -1,4 +1,61 @@
 package DataObjects;
 
 public class Stock {
+    String ticker;
+    String name;
+    String sector;
+    double price;
+    double transactionCost;
+    Currency currency;
+    String rating;
+    double dividentYield;
+    Market market;
+    Date lastUpdated
+
+    // getters
+    public String getTicker(){return this.ticker}
+    public String getName(){return this.name}
+    public String getSector(){return this.sector}
+    public double getPrice(){return this.price}
+    public double getTransactionCost(){return this.transactionCost}
+    public Currency getCurrency(){return this.currency}
+    public String getRating(){return this.rating}
+    public double getDividentYield(){return this.dividentYield}
+    public Market getMarket(){return this.market}
+    public Date getLastUpdate(){return this.lastUpdated}
+
+    // setters
+    public void setTicker(String ticker){this.ticker = ticker;}
+    public void setName(String name){this.name = name;}
+    public void setSector(String sector){this.sector = sector;}
+    public void setPrice(double price){this.price = price;}
+    public void setTransactionCost(double transactionCost){this.transactionCost = transactionCost;}
+    public void setCurrency(Currency currency){this.currency = currency;}
+    public void setRating(String rating){this.rating = rating;}
+    public void setDividentYield(double dividentYield){this.dividentYield = dividentYield;}
+    public void setMarket(Market market){this.market = market;}
+    public void setLastUpdated(var lastUpdated){this.lastUpdated = lastUpdated;}
+
+    // constructor
+    Stock(String ticker,
+    String name,
+    String sector,
+    double price,
+    double transactionCost;
+    Currency currency,
+    String rating,
+    double dividentYield,
+    Market market,
+    Date lastUpdated
+    ){
+        this.ticker = ticker;
+        this.name = name;
+        this.price = name;
+        this.transactionCost = transactionCost;
+        this.currency = currency;
+        this.rating = rating;
+        this.dividentYield = dividentYield;
+        this.market = market;
+        this.lastUpdated = lastUpdated;
+    }
 }

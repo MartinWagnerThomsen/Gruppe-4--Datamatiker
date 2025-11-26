0aefc8a38e274dba808878b188cbfae3fe431cfd
Nelsoon-ds <davidgaming108@gmail.com>
2025-11-26 11:29:11 +0100

    Merge conflicts

--- Investeringsklubben/src/Club.java
+++ Investeringsklubben/src/Club.java
@@ -1,14 +1,13 @@
 import Filehandling.CsvHandler;
-import Filehandling.FileHandler;
 import Users.Member;
 
 import java.util.ArrayList;
 
-import com.sun.tools.javac.Main;
 
 public class Club {
     static String adminUserName = "president@gmail.com";
-    String password = "Sauron";
+    static String password = "Sauron";
+    private final CsvHandler handler = new CsvHandler();
 
 
     public static void login() {
@@ -25,45 +24,26 @@ public class Club {
 
             } else {
                 System.out.println("Prøver at logge ind som medlem");
-    private final CsvHandler handler = new CsvHandler();
-    public void login(){
-
             }
-            // checker om brugeren ikke er presidenten
-            if (!adminUserName.equals(adminUserName)) {
-                System.out.println("Logger ind som medlem");
-                isLoggedIn = true;
+            System.out.println("1. Se oversigt over brugernes porteføljeværdi \n" +
+                    "2. Vis rangliste\n " +
+                    "3. Vis fordelinger på aktier og sektorer\n" +
+                    "4. Tilføj ny bruger\n" +
+                    "5. Fjern bruger\n" +
+                    "6. Log ud");
+        }}
 
-            } else {
-                System.out.println("Forkert brugernavn eller kodeord.");
-            }
+        public void logout () {
+
         }
+        public void switchUser () {
 
+        }
+        public static void main(String[] args){
+            Club investmentClub = new Club();
+            InvestmentClubFacade facade = new InvestmentClubFacade();
+            ArrayList<Member> activeMembers = facade.fetchUserData();
+            login();
 
-        System.out.println("1. Se oversigt over brugernes porteføljeværdi \n" +
-                "2. Vis rangliste\n " +
-                "3. Vis fordelinger på aktier og sektorer\n" +
-                "4. Tilføj ny bruger\n" +
-                "5. Fjern bruger\n" +
-                "6. Log ud");
+        }
     }
-
-    public void logout() {
-
-    }
-
-    public void switchUser() {
-
-    }
-    public static void main(String[] args) {
-        Club investmentClub = new Club();
-        InvestmentClubFacade facade = new InvestmentClubFacade();
-        ArrayList<Member> activeMembers = facade.fetchUserData();
-
-
-    }
-
-    public static void main(String[] args) {
-        login();
-    }
-}

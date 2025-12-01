71f4f62e6617c920e60971e2f5c47c69d04e61c3
Nelsoon-ds <davidgaming108@gmail.com>
2025-12-01 09:41:49 +0100

    Valutakonvertering implementeret i Club klassen.

--- Investeringsklubben/src/DataObjects/Currency.java
+++ Investeringsklubben/src/DataObjects/Currency.java
@@ -4,15 +4,14 @@ package DataObjects;
 import Filehandling.DataManager;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class Currency {
     private String baseCurr;
     private String quote;
     private double rate;
     private LocalDate lastUpdated;
-    private List<Currency> currencies = new ArrayList<>();
 
     public Currency(String baseCurr, String quote, double rate, LocalDate lastUpdated) {
         this.baseCurr = baseCurr;
@@ -30,16 +29,31 @@ public class Currency {
         return baseCurr;
     }
 
-    public double convertToDkk () {
-        DataManager manager = new DataManager();
-        this.currencies = manager.getCurrencies();
 
-        for (Currency element : currencies) {
-            if(element.getBaseCurr() == this.baseCurr) {
-                this.getRate() = element.getRate();
-            }
+    /**
+     * Find beløbet
+     * Find den relevante kurs pris  (fx. Euro)
+     * Multiplicer beløbet med kursen
+     * Rund det af til nærmeste decimal
+     * Overvej gebyrer
+     * @return
+     */
+    public void convertToDkk (Stock stock) {
+        // Vi har brug for at få vores rater
+        DataManager manager = new DataManager();
+        List<Currency> listOfCurrenciesAndRates = manager.getCurrencies();
+
+        // Så har vi brug for at gemme aktie kursen
+        String stockCurrency = stock.getCurrency();
+        System.out.println(stockCurrency);
+
+
+        for (Currency currency : listOfCurrenciesAndRates) {
         }
 
+    }
+
+    public static void main(String[] args) {
     }
 
 
